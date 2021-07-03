9507bd2bfd0e2cdf4d99a308bce3ebe7cdd728bb
Prasenjit20 <mailtoprasenjit.biswas@gmail.com>
2021-07-03 13:03:00 +0530

    Change the count in main

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "aws_instance" "myFirstInstance" {
   key_name = var.key_name
   instance_type = var.instance_type
   #security_groups= ["Public 1 SG"] (Need to check why not working)
+  count = 1
   tags= {
     Name = "terraform instance"
   }
